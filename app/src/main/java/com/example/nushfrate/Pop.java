890f6a012a241c5bd7b42d86cc2cf46e49507b55
Martyn00 <martinionescu11@gmail.com>
2020-07-14 18:15:39 +0300

    ferestre mici si altele... design nada... sper sa moara cine a inventat xml=)))))

--- app/src/main/java/com/example/nushfrate/Pop.java
+++ app/src/main/java/com/example/nushfrate/Pop.java
@@ -2,6 +2,7 @@ package com.example.nushfrate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +15,13 @@ public class Pop extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pop_up);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int)(width*.6),(int)(height*.4));
+
         Button btn = (Button) findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
