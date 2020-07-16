3d45da9713f12f50d6ef99dc6a293d2d9fabf4e4
Sebastian <litoiu.sebastian19@gmail.com>
2020-07-16 19:56:46 +0300

    sper

--- app/src/main/java/com/example/nushfrate/MainActivity.java
+++ app/src/main/java/com/example/nushfrate/MainActivity.java
@@ -1,30 +1,21 @@
 package com.example.nushfrate;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import java.nio.charset.MalformedInputException;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import static java.lang.Long.*;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, Login.LoginListener {
+public class MainActivity extends AppCompatActivity implements Login.LoginListener{
 
-    Money bani = new Money(1500);
-    private int code = 0;
-    private int code1 = 1;
-    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,106 +23,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
 
-        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE );
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
         boolean firsStart = prefs.getBoolean("firsStart", true);
 
         if (firsStart) {
-          showLoginDialog();
+            showLoginDialog();
         }
 
-        TextView theMoneyText = (TextView) findViewById(R.id.textView2);
-        theMoneyText.setText(String.valueOf(bani.getSum()));
-
-
-        ImageButton btnPierde = (ImageButton) findViewById(R.id.Pierde);
-        ImageButton btnPrimeste = (ImageButton) findViewById(R.id.Primeste);
-        ImageButton btnQuickAdd = (ImageButton) findViewById(R.id.Quickadd);
-
-        btnPierde.setOnClickListener(this);
-        btnPrimeste.setOnClickListener(this);
-        btnQuickAdd.setOnClickListener(this);
-
-
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navi);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+    private void showLoginDialog() {
+        Login login = new Login();
+        login.show(getSupportFragmentManager(), "login");
 
-    private void showLoginDialog(){
-       Login login = new Login();
-       login.show(getSupportFragmentManager(), "login");
-
-       SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
-       SharedPreferences.Editor editor = prefs.edit();
-       editor.putBoolean("firstStart", false);
-       editor.apply();
-    }
-
-    @Override
-    public void onClick(View view) {
-        TextView theMoneyText = (TextView) findViewById(R.id.textView2);
-        String number, prop;
-        switch (view.getId()){
-            case R.id.Pierde:
-                MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.crowd_boo);
-                mediaPlayer.start();
-                Intent intent = new Intent(this, Pop.class);
-                startActivityForResult(intent, code);
-                break;
-
-            case R.id.Primeste:
-                    MediaPlayer mediaPlayerL = MediaPlayer.create(this, R.raw.cha_ching);
-                    mediaPlayerL.start();
-                    Intent intentW = new Intent(this, Pop2.class);
-                    startActivityForResult(intentW, code1);
-
-
-                break;
-
-            case R.id.Quickadd:
-                bani.cresteBani(200);
-                theMoneyText.setText(String.valueOf(bani.getSum()));
-                prop = "Saracule, ai trecut de start, poftim 200";
-                MediaPlayer mediaPlayer200 = MediaPlayer.create(this, R.raw.cha_ching);
-                mediaPlayer200.start();
-                Toast.makeText(this, prop, Toast.LENGTH_SHORT).show();
-             break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + view.getId());
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode ==  code){
-            if(resultCode == RESULT_OK){
-                String return_text = data.getStringExtra(Intent.EXTRA_TEXT);
-                if(!return_text.isEmpty()){
-                    bani.scadeBani(parseLong(return_text));
-                    TextView theMoneyText = (TextView) findViewById(R.id.textView2);
-                    theMoneyText.setText(String.valueOf(bani.getSum()));
-                    if(bani.getSum() == 0){
-                        String prop = "Ai pierdut BOSS=(((";
-                        Toast.makeText(this, prop, Toast.LENGTH_SHORT).show();}
-
-
-                    }
-                }
-            }
-
-        if(requestCode == code1){
-            if(resultCode == RESULT_OK){
-                String return_text = data.getStringExtra(Intent.EXTRA_TEXT);
-                if(!return_text.isEmpty()){
-                    bani.cresteBani(Long.parseLong(return_text));
-                    TextView theMoneyText = (TextView) findViewById(R.id.textView2);
-                    theMoneyText.setText(String.valueOf(bani.getSum()));
-                }
-            }
-        }
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("firstStart", false);
+        editor.apply();
     }
 
     @Override
     public void applyText(String username) {
-        Toast.makeText(this,"Salut, " +  username, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Salut, " + username, Toast.LENGTH_SHORT).show();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeActivity();
+                            break;
+                        case R.id.nav_history:
+                            selectedFragment = new History();
+                            break;
+                        case R.id.nav_pay:
+                            selectedFragment = new Pay();
+                            break;
+                        case R.id.nav_scan:
+                            selectedFragment = new Scan();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
 }
+
 
