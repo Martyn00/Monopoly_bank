f6996b710b88f60f811dc8ea4894f3c2e8fd2228
Sebastian <litoiu.sebastian19@gmail.com>
2020-07-29 15:32:45 +0300

    iupi

---
+++ app/src/main/java/com/example/nushfrate/RandomPlayers.java
@@ -0,0 +1,17 @@
+package com.example.nushfrate;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RandomPlayers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.randomplayers);
+
+
+    }
+
+}
